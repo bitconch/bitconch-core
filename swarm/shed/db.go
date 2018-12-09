f1fe4b9114085316139bbd8525b32af64535f2fb
wugaofeng <wgf_simple@163.com>
2018-12-09 21:35:40 +0800

    Revert "update package"
    This reverts commit 94e48d41dc72748b3dce749ff90643363d18d011.

--- swarm/shed/db.go
+++ swarm/shed/db.go
@@ -23,7 +23,7 @@
 package shed
 
 import (
-	"github.com/Bitconch/BUS/metrics"
+	"github.com/ethereum/go-ethereum/metrics"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/iterator"
 	"github.com/syndtr/goleveldb/leveldb/opt"
--- swarm/shed/field_struct.go
+++ swarm/shed/field_struct.go
@@ -17,7 +17,7 @@
 package shed
 
 import (
-	"github.com/Bitconch/BUS/rlp"
+	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
