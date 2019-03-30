0eda392bbb26f4c9fbc6fe99b9edf08218f49c7b
bluewebgeek <BLUE.WEB.GEEK@gmail.com>
2019-03-31 00:01:15 +0800

    fix path error for run1.sh

--- run1.sh
+++ run1.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #cd ./go/src/github.com/caesarchad/rustelo-rust/soros && cargo build --all && ./run.sh
 python3 deploy.py
-cd buffett && ./demo/setup.sh && ./demo/leader.sh
+cd ~/go/src/github.com/bitconch/bus/vendor/rustelo-rust/buffett && ./demo/setup.sh && ./demo/leader.sh
