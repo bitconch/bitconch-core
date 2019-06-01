28b820e20173a516267b710ec8f90c93606b1876
Caesar Chad <BLUE.WEB.GEEK@gmail.com>
2019-06-01 10:36:48 +0800

    remove blazelib to its own repo

--- blazelib/src/cuda-crypt/common.cu
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-#ifndef COMMON_CU
-#define COMMON_CU
-
-#define BLOCK_SIZE (4 * 1024)
-
-#endif
--- blazelib/src/gpu-common.mk
+++
@@ -1,6 +0,0 @@
-NVCC:=nvcc
-GPU_ARCH:=compute_35
-GPU_CFLAGS:=--gpu-code=sm_37,sm_50,sm_61,sm_70,$(GPU_ARCH) --gpu-architecture=$(GPU_ARCH)
-CFLAGS_release:=--ptxas-options=-v $(GPU_CFLAGS) -O3 -Xcompiler "-Wall -Werror -fPIC -Wno-strict-aliasing"
-CFLAGS_debug:=$(CFLAGS_release) -g
-CFLAGS:=$(CFLAGS_$V)
