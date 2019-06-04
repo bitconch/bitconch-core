34a547ac2c0ebbbb1db7dae40f2c6042a626ed97
Caesar Chad <BLUE.WEB.GEEK@gmail.com>
2019-06-04 14:10:29 +0800

    add new pacakge crypto

---
+++ buffett2/crypto/src/lib.rs
@@ -0,0 +1,18 @@
+pub mod hash;
+pub mod signature;
+extern crate bs58;
+
+extern crate rayon;
+extern crate ring;
+
+extern crate untrusted;
+extern crate buffett_interface;
+extern crate sha2;
+extern crate generic_array;
+#[macro_use]
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
+extern crate rand;
