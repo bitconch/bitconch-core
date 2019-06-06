2192781c3a8ea02f78a48037eefee02345f3fb72
caesar chad <blue.web.geek@gmail.com>
2019-06-06 16:41:09 +0800

    remove buffett2 folder

--- buffett2/budget/src/payment.rs
+++
@@ -1,8 +0,0 @@
-use buffett_interface::pubkey::Pubkey;
-
-
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
-pub struct Payment {
-    pub balance: i64,
-    pub to: Pubkey,
-}
--- buffett2/budget/src/seal.rs
+++
@@ -1,7 +0,0 @@
-use chrono::prelude::*;
-
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
-pub enum Seal {
-    Timestamp(DateTime<Utc>),
-    Signature,
-}
