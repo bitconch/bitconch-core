f3dc31f2eb976454426d943db63aa185bba3ecbb
Caesar Chad <BLUE.WEB.GEEK@gmail.com>
2019-06-10 13:47:29 +0800

    change default influxdb host to bitconch.org

--- buffett2/metrics/src/metrics.rs
+++ buffett2/metrics/src/metrics.rs
@@ -35,7 +35,7 @@ impl DbWriter {
 
     fn build_client() -> Option<influxdb::Client> {
         let host = env::var("INFLUX_HOST")
-            .unwrap_or_else(|_| "https://127.0.0.1:8086".to_string());
+            .unwrap_or_else(|_| "https://dashboard.bitconch.org".to_string());
         let db = env::var("INFLUX_DATABASE").unwrap_or_else(|_| "scratch".to_string());
         let username = env::var("INFLUX_USERNAME").unwrap_or_else(|_| "scratch_writer".to_string());
         let password = env::var("INFLUX_PASSWORD").unwrap_or_else(|_| "topsecret".to_string());
