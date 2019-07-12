6bb0644f7246670f203b1347bbf75c3d5aea2843
aya <2581015450@qq.com>
2019-07-12 15:09:42 +0800

    bash files run1, run2, run3, run4 are no longer needed

--- run1.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#cd ./go/src/github.com/caesarchad/rustelo-rust/soros && cargo build --all && ./run.sh
-python3 deploy.py
-cd ~/go/src/github.com/bitconch/bus/vendor/rustelo-rust/buffett && ./demo/setup.sh && ./demo/leader.sh
--- run3.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-cd ~/go/src/github.com/bitconch/bus/vendor/rustelo-rust/buffett && ./demo/validator-x.sh
