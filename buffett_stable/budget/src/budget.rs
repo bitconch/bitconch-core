ca2b2d1654d45eb0ac835da2aa8a415d3abd9a65
cc-cara <cara18778833932@163.com>
2019-07-29 13:51:44 +0800

    add comments for uffett_stable/budget

--- buffett_stable/budget/src/budget.rs
+++ buffett_stable/budget/src/budget.rs
@@ -9,7 +9,7 @@ use std::mem;
 
 #[repr(C)]
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
-/// define a public enueration of Budget,its filed are Pay, After, Or, And
+/// define a public enueration of Budget,its variants are Pay, After, Or, And
 pub enum Budget {
     
     Pay(Payment),
@@ -28,12 +28,12 @@ pub enum Budget {
 impl Budget {
     
     
-/// define a public final_payment method on the Budget struct
-/// with the parameter Budget's filed and the type of return value is Option<Payment>
+/// define a public final_payment method on the Budget enum
+/// with the parameter Budget's variants and the type of return value is Option<Payment>
     pub fn final_payment(&self) -> Option<Payment> {
 /// match with Budget
         match self {
-/// if Budget filed is Pay's payment, execute the branch and clone payment return it 
+/// if Budget variants is Pay's payment, execute the branch and clone payment return it 
             Budget::Pay(payment) => Some(payment.clone()),
 /// else return None
             _ => None,
@@ -41,21 +41,24 @@ impl Budget {
     }
 
     
-/// define a public verify method on the Budget struct
+/// define a public verify method on the Budget enum
 /// with the parameter of Budget's filed and spendable_balance, the type of return value is bool
     pub fn verify(&self, spendable_balance: i64) -> bool {
 /// match with Budget
         match self {
 /// if Budget filed is Pay's payment, or After'payment, or And'payment
-/// execute the branch and return ture or false
+/// execute the branch and return true or false
             Budget::Pay(payment) | Budget::After(_, payment) | Budget::And(_, _, payment) => {
                 payment.balance == spendable_balance
             }
+/// if Budget filed is Or((Condition, Payment), (Condition, Payment))
+/// then execute this branch and returns true or flase
             Budget::Or(a, b) => a.1.balance == spendable_balance && b.1.balance == spendable_balance,
         }
     }
 
     
+/// define a public apply_seal method on the Budget enum
     pub fn apply_seal(&mut self, seal: &Seal, from: &Pubkey) {
         let new_budget = match self {
             Budget::After(condition, payment) if condition.is_satisfied(seal, from) => {
