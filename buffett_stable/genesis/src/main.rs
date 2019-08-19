6c2329536e719a26dcc9d16ad5e1bf9d9f6bcc78
cc-cara <cara18778833932@163.com>
2019-08-19 14:24:22 +0800

    add comments for gensis

--- buffett_stable/genesis/src/main.rs
+++ buffett_stable/genesis/src/main.rs
@@ -40,27 +40,32 @@ fn main() -> Result<(), Box<error::Error>> {
                 .help("Use directory as a dedicated ledgerbook path"),
         ).get_matches();
 
-    /// getting a typed value T
-    /// where T implements std::str::FromStr or exiting upon error, instead of returning a Result type
+    /// getting a typed value of i64
     let tokens = value_t_or_exit!(matches, "tokens", i64);
     /// get the value of "ledger"
     let ledger_path = matches.value_of("ledger").unwrap();
 
+    /// if Stream enumeration variants of Stdin is tty, then output the error message and exit the program
     if is(Stream::Stdin) {
         eprintln!("nothing found on stdin, expected a json file");
         exit(1);
     }
 
+    /// new a String
     let mut buffer = String::new();
+    /// reference to "mut buffer" to read all bytes until EOF in this source, appending them to buf
     let num_bytes = stdin().read_to_string(&mut buffer)?;
     if num_bytes == 0 {
         eprintln!("empty file on stdin, expected a json file");
         exit(1);
     }
 
+    /// parse the string of "buffer" into vector
     let pkcs8: Vec<u8> = serde_json::from_str(&buffer)?;
+    /// generation instances of Mint by through tokens and pkcs8
     let mint = Mint::new_with_pkcs8(tokens, pkcs8);
 
+    /// opens or creates a LedgerWriter in ledger_path directory
     let mut ledger_writer = LedgerWriter::open(&ledger_path, true)?;
     ledger_writer.write_entries(mint.create_entries())?;
 
