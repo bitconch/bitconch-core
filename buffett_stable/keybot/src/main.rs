aea8767d023b7afc3e58daa6dcc0d212496cf72b
cc-cara <cara18778833932@163.com>
2019-08-20 10:14:40 +0800

    add comments for keybot

--- buffett_stable/keybot/src/main.rs
+++ buffett_stable/keybot/src/main.rs
@@ -8,7 +8,12 @@ use clap::{App, Arg};
 use buffett_core::wallet::gen_keypair_file;
 use std::error;
 
+/// declare main function, return value type is Result <T, E>, where T is (), E is Box <error:: Error>
 fn main() -> Result<(), Box<error::Error>> {
+    /// creates a new instance of an application named "buffettt_tokenbot",
+    /// automatically set the version of the "buffettt_tokenbot" application,
+    /// starts the parsing process, upon a failed parse an error will be displayed to the user 
+    /// and the process will exit with the appropriate error code
     let matches = App::new("buffettt_tokenbot")
         .version(crate_version!())
         .arg(
