d47c9683b7fd7ebaf109391c3b5a28d0855d06f5
cc-cara <cara18778833932@163.com>
2019-09-16 15:55:54 +0800

    adding new file to deploy node service

---
+++ deploy-morgan.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Will deploy nightly Bitconch chain, codename morgan 
+"""
+import logging
+import stat
+import shutil
+import os, re, argparse, sys,crypt
+import getpass
+import click
+from subprocess import Popen, check_call, PIPE, check_output, CalledProcessError
+from shutil import copy2, copytree, rmtree
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+
+def rmtree_onerror(self, func, file_path, exc_info):
+    """
+    Error handler for ``shutil.rmtree``.
+    If the error is due to an access error (read only file)
+    it attempts to add write permission and then retries.
+    If the error is for another reason it re-raises the error.
+    Usage : ``shutil.rmtree(path, onerror=onerror)`` 
+    """
+    logging.warning(str(exc_info))
+    logging.warning("rmtree error,check the file exists or try to chmod the file,then retry rmtree action.")
+    os.chmod(file_path, stat.S_IWRITE) #chmod to writeable
+    if os.path.isdir(file_path):
+        #file exists
+       func(file_path)
