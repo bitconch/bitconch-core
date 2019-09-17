7095f624e704a4c9ad5dc6e3484b5ea74ebfb230
cc-cara <cara18778833932@163.com>
2019-09-17 13:58:20 +0800

    adding new file to deploy node service

--- deploy-morgan.py
+++ deploy-morgan.py
@@ -117,3 +117,10 @@ def build(rust_version,cargoFeatures,release=False):
         "x86_64-unknown-linux-gnu",
         "x86_64-apple-darwin"
     ]
+
+    prefix = {
+        "x86_64-pc-windows-gnu": "x86_64-w64-mingw32-",
+        "x86_64-unknown-linux-musl": "x86_64-linux-musl-",
+        "x86_64-unknown-linux-gnu": "x86_64-linux-gnu-",
+         "x86_64-apple-darwin": ""
+    }
